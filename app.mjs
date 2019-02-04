72b8f80697bcf1f8c9cd7dd1b2ad81167896d2ba
tobi <tobi.akiny@gmail.com>
2019-02-04 19:17:10 +0000

    added hello worlds

--- app.mjs
+++ app.mjs
@@ -1,3 +1,8 @@
 import Serve from "./serve";
 
+const server = new Serve("C:\\Users\\tobia\\Documents\\note");
+server.listen("127.0.0.1");
+console.log(server.router);
+// server.router.get("*", (req, res) => res.end("WOAH"));
+
 console.log(Serve);
