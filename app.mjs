33afaf1e76d8127c0ecf7c5b7c234827bedae50e
tobi <tobi.akiny@gmail.com>
2019-02-25 02:22:37 +0000

    updated app file

--- app.mjs
+++ app.mjs
@@ -1,8 +1,13 @@
 import Serve from "./serve";
 
-const server = new Serve("C:\\Users\\tobia\\Documents\\note");
+const server = new Serve({
+    templates: "../templates/"
+});
 server.listen("127.0.0.1");
-console.log(server.router);
+
+server.router.get("/", (req, res) => {
+    server.render("index", req, res, {user: "Tobi"});
+});
 // server.router.get("*", (req, res) => res.end("WOAH"));
 
 console.log(Serve);
