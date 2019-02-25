ccd62a084d404678651116ea70f634d5e187cdfb
Tobi <tobi.akiny@gmail.com>
2019-02-25 02:23:22 +0000

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -8,6 +8,5 @@ server.listen("127.0.0.1");
 server.router.get("/", (req, res) => {
     server.render("index", req, res, {user: "Tobi"});
 });
-// server.router.get("*", (req, res) => res.end("WOAH"));
 
 console.log(Serve);
