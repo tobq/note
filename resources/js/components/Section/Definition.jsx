09230aabdb43e24fa4cceb51c45d67d59105081b
tobi <tobi.akiny@gmail.com>
2019-02-27 11:43:00 +0000

    added some keyboard functionality for definition

--- resources/js/components/Section/Definition.jsx
+++ resources/js/components/Section/Definition.jsx
@@ -21,10 +21,55 @@ export default class Definition extends React.Component {
         };
     }
 
+    focus() {
+        this.title.current.focus();
+    }
+
+    componentDidMount() {
+        this.body.current.focus()
+    }
+
+    onTitleKeyDown(e) {
+        if (e.keyCode === 8) {
+            if (this.title.current.textContent.length === 0) {
+                e.preventDefault();
+                this.props.onRemove(this);
+            }
+        } else if (e.keyCode === 13) {
+            e.preventDefault();
+            this.body.current.focus();
+        }
+    }
+
+    onBodyKeyDown(e) {
+        if (e.keyCode === 8) {
+            if (this.body.current.textContent.length === 0) {
+                e.preventDefault();
+                this.props.onCreate(this, this.value.title).then(e =>
+                    this.props.onRemove(this));
+            }
+        } else if (e.keyCode === 13) {
+            e.preventDefault();
+            this.props.onCreate(this);
+        }
+    }
+
     render() {
-        return <div className="section point definition">
-            <div className="definition-title" contentEditable tabIndex={0} ref={this.title}>{this.props.title}</div>
-            <div className="definition-body" contentEditable tabIndex={0} ref={this.body}>{this.props.children}</div>
+        return <div className="definition">
+            <div
+                className="definition-title"
+                contentEditable suppressContentEditableWarning
+                tabIndex={0}
+                onKeyDown={e => this.onTitleKeyDown(e)}
+                ref={this.title}
+            >{this.props.title}</div>
+            <div
+                className="definition-body"
+                contentEditable suppressContentEditableWarning
+                tabIndex={0}
+                onKeyDown={e => this.onBodyKeyDown(e)}
+                ref={this.body}
+            >{this.props.children}</div>
         </div>;
     }
 }
