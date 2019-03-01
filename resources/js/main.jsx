3c3dc55a15daf6f6b21c24596542382c178c8666
tobi <tobi.akiny@gmail.com>
2019-03-01 01:09:55 +0000

    refactored main js file to accomodate changes to Note module

--- resources/js/main.jsx
+++ resources/js/main.jsx
@@ -1,42 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "../sass/main.scss";
-import Section from "./components/Section/Section";
-import Definition from "./components/Section/Definition";
-import SectionBuilder from "./components/Section/util/SectionBuilder";
+import parseJSON from "./util/parseJSON";
 
 const nav = document.querySelector("nav");
 const header = document.querySelector("header");
 const NAV_STICK_CLASSNAME = "stick";
+const bodyContent = document.getElementById("body-content");
+const defaultConfig = {
+    type: "note",
+    title: "TOP",
+    body: [
+        {
+            type: "note",
+            title: "Section",
+            body: [
+                {type: "point", body: "First Point"},
+                {type: "point", body: "Second Point"},
+                {type: "point", body: "Another Point"},
+            ]
+        }, {
+            type: "definition",
+            title: "definition",
+            body: "An example of a definition, placed in a sub-note, as a child"
+        },
+        {type: "point", body: "First Point"},
+        {type: "point", body: "Second Point"},
+        {type: "point", body: "Another Point"},
+    ]
+};
+const note = parseJSON(defaultConfig);
+const noteComponent = note.getComponent();
+
+ReactDOM.render(noteComponent, bodyContent);
+window.addEventListener("keydown", e => {
+    if (e.keyCode === 27) console.log(noteComponent.ref.current.value);
+});
+
 document.addEventListener("scroll", event => {
     if (header.getBoundingClientRect().bottom < 0) nav.classList.add(NAV_STICK_CLASSNAME);
     else nav.classList.remove(NAV_STICK_CLASSNAME)
 });
-
-let ref = React.createRef();
-ReactDOM.render(<SectionBuilder ref={ref} defaultValue={{
-    type: "section",
-    title: "TOP",
-    sections: [
-        {
-            type: "section",
-            title: "Section",
-            sections: [
-                {type: "point", value: "First Point"},
-                {type: "point", value: "Second Point"},
-                {type: "point", value: "Another Point"},
-            ]
-        }, {
-            type: "definition",
-            title: "Definition",
-            value: "An example of a definition placed in a section as a child"
-        },
-        {type: "point", value: "First Point"},
-        {type: "point", value: "Second Point"},
-        {type: "point", value: "Another Point"},
-    ]
-}}/>, document.getElementById("body-content"));
-console.log(ref.current.value);
-
-//
-//
